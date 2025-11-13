4162fbc3d596192f9d0ccf50df38fd9101287476
barakamukungu <bethelybaraka@gmail.com>
2025-11-13 17:41:08 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,19 +5,24 @@ const blogRoutes = require('./routes/blog-routes');
 const userRoutes = require('./routes/user-routes');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/blogs', blogRoutes);
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('Blog API (in-memory + JSON files)'));
+const clientBuildPath = path.join(__dirname, 'client', 'build');
+app.use(express.static(clientBuildPath));
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
+
+app.get('/', (req, res) => res.send('Blog API (in-memory + JSON files)'));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
