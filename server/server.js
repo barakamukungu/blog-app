858c52c4b277b5fa04a349802f1ebfeb0a229848
barakamukungu <bethelybaraka@gmail.com>
2025-11-13 16:04:05 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Blog API (in-memory + JSON files)'));
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
